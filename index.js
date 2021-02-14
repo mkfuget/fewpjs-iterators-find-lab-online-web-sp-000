73239755513184f270c0b2f7770905cc414b6be7
Michael Fuget <michaelfuget@gmail.com>
2021-02-14 07:10:50 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(array){
+  let out = array.find( s => s.result === "W")
+  return (out === undefined ? undefined : out.year)
+
+}
